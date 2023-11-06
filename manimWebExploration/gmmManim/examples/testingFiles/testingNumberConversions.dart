9a74cac420b969b35bcc7ebb9cffa9f55934a2e1
shreyu palley <rockingshreyu@gmail.com>
2023-11-06 18:10:44 +0530

    Added tests for number conversions, and working on creating Decimal number open source for manim web

---
+++ manimWebExploration/gmmManim/examples/testingFiles/testingNumberConversions.dart
@@ -0,0 +1,11 @@
+
+
+void main() {
+  double a = 3.1415;
+  double b = 9999.123;
+  double c = 0.00000000012;
+
+  print(a.toStringAsPrecision(3));
+  print(b.toStringAsPrecision(3));
+  print(c.toStringAsPrecision(3));
+}
