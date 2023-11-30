ce951242907d0b26506b18045295bb51549f7373
akshat <akshat.sanghvi@students.iiit.ac.in>
2023-11-30 19:01:27 +0530

    small changes

--- manimWebExploration/pyManim/manim/1.py
+++ manimWebExploration/pyManim/manim/1.py
@@ -4,12 +4,16 @@ from sklearn.mixture import GaussianMixture
 
 class GMMAnimation(ThreeDScene):
     def construct(self):
-        # Create 3D axes
-        axes = ThreeDAxes()
-        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
+        axes = ThreeDAxes(
+            x_range=[-7, 7],
+            y_range=[-7, 7],
+            z_range=[-15, 15],
+        )
+        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES, zoom=0.8)
+        self.begin_ambient_camera_rotation(rate=0.1)
+
         self.add(axes)
 
-        # Generate 2D data
         np.random.seed(42)
         data = np.concatenate([
             np.random.normal(loc=[-2, -2], scale=0.5, size=(50, 2)),
@@ -17,28 +21,25 @@ class GMMAnimation(ThreeDScene):
             np.random.normal(loc=[-3, 3], scale=0.8, size=(50, 2)),
         ])
 
-        # Fit Gaussian Mixture Model
         gmm = GaussianMixture(n_components=3, covariance_type='full', random_state=42)
         gmm.fit(data)
 
-        # Create Gaussian surfaces based on initial GMM parameters
         gaussian_surfaces = [
             GaussianSurface(mean, covariance, weight).create_surface()
             for mean, covariance, weight in zip(gmm.means_, gmm.covariances_, gmm.weights_)
         ]
 
-        # Display Gaussian surfaces
         self.play(*[Create(surface) for surface in gaussian_surfaces])
+
         num_iter = 5
-        # Animate GMM parameters
+
         for i in range(num_iter):
-            # Update GMM for the next iteration
             gmm.fit(data)
 
-            # Update Gaussian surfaces based on new GMM parameters
             transforms = []
             for surface, mean, covariance, weight in zip(gaussian_surfaces, gmm.means_, gmm.covariances_, gmm.weights_):
-                new_surface = GaussianSurface(mean + np.random.normal(loc=0, scale=0.6, size=2), covariance, weight).create_surface()
+                # also adding rotation like we did for the axes below
+                new_surface = GaussianSurface(mean + np.random.normal(loc=0, scale=0.6, size=2), covariance, weight).create_surface()#.copy().rotate(PI/4, axis=axes.c2p(0, 1, 0))
                 transforms.append(Transform(surface, new_surface))
 
             # transforms = [[
@@ -46,13 +47,10 @@ class GMMAnimation(ThreeDScene):
             #     for surface, mean, covariance, weight in zip(gaussian_surfaces, gmm.means_, gmm.covariances_, gmm.weights_)
             # ]]
 
-            # Apply transformations simultaneously
-            rotated_axes = axes.copy().apply_matrix([[np.cos(PI/4), -np.sin(PI/4), 0], [np.sin(PI/4), np.cos(PI/4), 0], [0, 0, 1]])
-            transforms.append(Transform(axes, rotated_axes))
+            # rotated_axes = axes.copy().apply_matrix([[np.cos(PI/4), -np.sin(PI/4), 0], [np.sin(PI/4), np.cos(PI/4), 0], [0, 0, 1]])
+            # transforms.append(Transform(axes, rotated_axes))
             self.play(*transforms)
 
-            # Wait for a short duration
-            # self.wait(0.5)
 
 class GaussianSurface(ThreeDVMobject):
     def __init__(self, mean, covariance, weight, **kwargs):
@@ -80,7 +78,7 @@ class GaussianSurface(ThreeDVMobject):
         delta = np.array([u - self.mean[0], v - self.mean[1]])
         exponent = -0.5 * delta.T @ np.linalg.inv(self.covariance) @ delta
         normalization = 10
-        return -1 + self.weight * normalization * np.exp(exponent)
+        return 2 + self.weight * normalization * np.exp(exponent)
 
     def update(self, new_mean, covariance):
         # Update Gaussian parameters
