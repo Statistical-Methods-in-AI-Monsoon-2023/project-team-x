178af28c95a66baa636a38f473c253fdce059b75
akshat <akshat.sanghvi@students.iiit.ac.in>
2023-11-30 17:52:13 +0530

    using gmm to update gaussians

--- manimWebExploration/pyManim/manim/1.py
+++ manimWebExploration/pyManim/manim/1.py
@@ -1,58 +1,76 @@
 from manim import *
 import numpy as np
+from sklearn.mixture import GaussianMixture
 
-class GaussianSumEvolution(ThreeDScene):
+class GMMAnimation(ThreeDScene):
     def construct(self):
-        # Create 3D axes
         axes = ThreeDAxes()
         self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
         self.add(axes)
 
-        # Initial Gaussians
-        initial_gaussians = [
-            Gaussian(mean=[-2, -2], variance=0.5),
-            Gaussian(mean=[2, 2], variance=0.5),
-            Gaussian(mean=[-3, 3], variance=0.8),
+        np.random.seed(42)
+        data = np.concatenate([
+            np.random.normal(loc=[-2, -2], scale=0.5, size=(50, 2)),
+            np.random.normal(loc=[2, 2], scale=0.5, size=(50, 2)),
+            np.random.normal(loc=[-3, 3], scale=0.8, size=(50, 2)),
+        ])
+
+        gmm = GaussianMixture(n_components=3, covariance_type='full', random_state=42)
+        gmm.fit(data)
+
+        gaussian_surfaces = [
+            GaussianSurface(mean, covariance, weight).create_surface()
+            for mean, covariance, weight in zip(gmm.means_, gmm.covariances_, gmm.weights_)
         ]
 
-        # Create Gaussian surfaces for the initial state
-        gaussian_surfaces = [gaussian.create_surface() for gaussian in initial_gaussians]
         self.play(*[Create(surface) for surface in gaussian_surfaces])
+        num_iter = 5
+        for i in range(num_iter):
+            gmm.fit(data)
 
-        # Evolution over time
-        for _ in range(3):
-            # Update Gaussians for the next time step
-            for gaussian in initial_gaussians:
-                gaussian.update()
+            if (i <  num_iter - 2):
+                transforms = [
+                    Transform(surface, GaussianSurface(mean + np.random.normal(loc=0, scale=1, size=2), covariance, weight).create_surface())
+                    for surface, mean, covariance, weight in zip(gaussian_surfaces, gmm.means_, gmm.covariances_, gmm.weights_)
+                ]
+            else:
+                transforms = [
+                    Transform(surface, GaussianSurface(mean, covariance, weight).create_surface())
+                    for surface, mean, covariance, weight in zip(gaussian_surfaces, gmm.means_, gmm.covariances_, gmm.weights_)
+                ]
 
-            # Smoothly transition between surfaces
-            self.play(*[Transform(surface, gaussian.create_surface()) for surface, gaussian in zip(gaussian_surfaces, initial_gaussians)])
-            self.wait(1)
+            self.play(*transforms)
 
-class Gaussian:
-    def __init__(self, mean, variance):
+            self.wait(0.3)
+
+class GaussianSurface(ThreeDVMobject):
+    def __init__(self, mean, covariance, weight, **kwargs):
+        super().__init__(**kwargs)
+        self.resolution = (40, 40)
         self.mean = mean
-        self.variance = variance
-
-    def update(self):
-        # Update the Gaussian for the next time step (for example, change mean or variance)
-        self.mean[0] += 0.5
-        self.mean[1] += 0.5
-        self.variance += 0.1
+        self.covariance = covariance
+        self.weight = weight
 
     def create_surface(self):
-        resolution = (42, 42)
         u_range = (-6, 6)
         v_range = (-6, 6)
 
         return Surface(
             lambda u, v: np.array([u, v, self.function(u, v)]),
-            resolution=resolution,
+            resolution=self.resolution,
             u_range=u_range,
             v_range=v_range,
-            checkerboard_colors=[BLUE_D, BLUE_E],
+            color=interpolate_color(WHITE, BLUE, 0.7),
+            shade_in_3d=True,
         )
 
     def function(self, u, v):
-        # Gaussian function
-        return np.exp(-(((u - self.mean[0]) ** 2) + ((v - self.mean[1]) ** 2)) / (2 * self.variance**2))
+        delta = np.array([u - self.mean[0], v - self.mean[1]])
+        exponent = -0.5 * delta.T @ np.linalg.inv(self.covariance) @ delta
+        normalization = 7
+        return self.weight * normalization * np.exp(exponent)
+
+    def update(self, new_mean, covariance):
+        self.covariance = covariance
+
+GMMAnimation().render()
